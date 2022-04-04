40c4d9c1b41c25d605558977c1de0bcaf1609f23
elimau <saltydogau@gmail.com>
2022-04-05 09:34:28 +1000

    impl tests for rules

--- src/nextGenerationRules/rules.ts
+++ src/nextGenerationRules/rules.ts
@@ -1,9 +1,16 @@
+import { CellStateType } from '../interfaces'
+import { CELL_STATE } from '../constants'
 
 export const sum = (a: number, b: number) => {
   return a + b
 }
 
-// export const calculateCellValue = (numLiveNeighbours) => {
-//   let result = null
-//   return result
-// }
+export const calculateCellValue = (currentState: CellStateType, numLiveNeighbours: number): CellStateType => {
+  let result = CELL_STATE.DEAD as CellStateType // todo: impl
+  return result
+}
+
+export const getNumLiveNeighbours = (boardState: CellStateType[][], cellX: number, cellY: number): number => {
+  let result = 0 // todo: impl
+  return result
+}
