a80325abb4c022bec42826b6d486faf3de9d3c6b
elimau <saltydogau@gmail.com>
2022-04-05 17:20:52 +1000

    impl rule 5

--- src/nextGenerationRules/rules.ts
+++ src/nextGenerationRules/rules.ts
@@ -15,35 +15,79 @@ export const calculateCellValue = ({
   let result
   switch (true) {
     // rule 1
+    // @ts-ignore: Fallthrough case in switch
     case (currentState === LIVE && numLiveNeighbours < 2): {
       result = DEAD
-      break
     }
     // rule 2
+    // @ts-ignore: Fallthrough case in switch
     case (currentState === LIVE && 2 <= numLiveNeighbours && numLiveNeighbours <= 3): {
       result = LIVE
-      break
     }
     // rule 3
+    // @ts-ignore: Fallthrough case in switch
     case (currentState === LIVE && 3 < numLiveNeighbours): {
       result = DEAD
-      break
     }
     // rule 4
+    // @ts-ignore: Fallthrough case in switch
     case (currentState === DEAD): {
       result = (numLiveNeighbours === 3) ? LIVE : DEAD
-      break
     }
-    // // rule 5
-    // case (
-    //   position.x === 0 || position.x === BOARD_SIZE.x - 1 ||
-    //   position.y === 0 || position.y === BOARD_SIZE.y - 1
-    // ): {
-    //     result = (numLiveNeighbours === 3) ? LIVE : DEAD
-    //     break
-    //   }
+    // rule 5
+    // @ts-ignore: Fallthrough case in switch
+    case (
+      position.x === 0 || position.x === BOARD_SIZE.x - 1 ||
+      position.y === 0 || position.y === BOARD_SIZE.y - 1
+    ): {
+        let neighbourStates: CellStateType[] = []
+        if (
+          position.x === 0 &&
+          position.y > 0 && position.y < BOARD_SIZE.y - 1
+        ) {
+          neighbourStates.push(
+            boardState[BOARD_SIZE.x - 1][position.y - 1],
+            boardState[BOARD_SIZE.x - 1][position.y],
+            boardState[BOARD_SIZE.x - 1][position.y + 1],
+          )
+        } else if (
+          position.x === BOARD_SIZE.x - 1 &&
+          position.y > 0 && position.y < BOARD_SIZE.y - 1
+        ) {
+          neighbourStates.push(
+            boardState[0][position.y - 1],
+            boardState[0][position.y],
+            boardState[0][position.y + 1],
+          )
+        } else if (
+          position.y === 0 &&
+          position.x > 0 && position.x < BOARD_SIZE.x - 1
+        ) {
+          neighbourStates.push(
+            boardState[position.x - 1][BOARD_SIZE.y - 1],
+            boardState[position.x][BOARD_SIZE.y - 1],
+            boardState[position.x + 1][BOARD_SIZE.y - 1],
+          )
+        } else if (
+          position.y === BOARD_SIZE.y - 1 &&
+          position.x > 0 && position.x < BOARD_SIZE.x - 1
+        ) {
+          neighbourStates.push(
+            boardState[position.x - 1][0],
+            boardState[position.x][0],
+            boardState[position.x + 1][0],
+          )
+        }
+        if (neighbourStates.length === 3 && neighbourStates.every(s => s === LIVE)) {
+          result = LIVE
+        }
+
+      }
+    // eslint-disable-next-line: no-fallthrough 
     default:
-      throw new Error('unexpected scenario')
+      if (!result) {
+        throw new Error('unexpected scenario')
+      }
   }
   return result
 }
